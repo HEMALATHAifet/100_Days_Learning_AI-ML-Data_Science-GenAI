83fdb84440dca2eb47f7b28f3a823a6ec3280a64
A.HEMALATHA <95863646+HEMALATHAifet@users.noreply.github.com>
2025-12-16 19:18:14 +0530

    Create 7.py

---
+++ python_Programs/7.py
@@ -0,0 +1,8 @@
+#write a python program to check whether a number is positive,negative,zero
+num=int(input("enter a number: "))
+if num>0:
+  print(num,"is positive")
+elif num<0:
+  print(num,"is negative")
+else:
+  print(num,"is zero")
