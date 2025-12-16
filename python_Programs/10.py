b43471881c768f4abb309c3bf102f507a1e291c1
A.HEMALATHA <95863646+HEMALATHAifet@users.noreply.github.com>
2025-12-16 19:26:45 +0530

    Create 10.py

---
+++ python_Programs/10.py
@@ -0,0 +1,6 @@
+#write a python program to check whether the given character is vowel or not
+char=input("enter a character: ")
+if char=='a' or char=='e' or char=='i' or char=='o' or char=='u':
+  print(char,"is vowel")
+else:
+  print(char,"is not vowel")
