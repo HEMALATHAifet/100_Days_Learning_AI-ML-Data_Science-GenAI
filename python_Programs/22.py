aef564f5012a997f3899b203a1bad210fe52cc3e
A.HEMALATHA <95863646+HEMALATHAifet@users.noreply.github.com>
2025-12-17 15:51:57 +0530

    Add program to check if character is a number

---
+++ python_Programs/22.py
@@ -0,0 +1,6 @@
+#WAP TO CHECK THE GIVEN CHARACTER IS NUMBER CHARACTER OR NOT
+char=input("enter character")
+if '0'<=char<='9':
+  print("it is a number character")
+else:
+  print("it is not a number character")
