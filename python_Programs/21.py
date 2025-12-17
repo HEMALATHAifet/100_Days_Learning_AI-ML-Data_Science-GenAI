23982233a98b44091fc05419a6d2c29ac9622352
A.HEMALATHA <95863646+HEMALATHAifet@users.noreply.github.com>
2025-12-17 15:50:43 +0530

    Add program to check for special characters
    This program checks if the given character is a special character.

---
+++ python_Programs/21.py
@@ -0,0 +1,6 @@
+#WAP TO CHECK THE GIVEN CHARACTER IS SPECIAL CHARACTER OR NOT
+char=input("enter character")
+if 'A'<=char<='Z' or 'a'<=char<='z' or '0'<=char<='9':
+  print("it is not a special character")
+else:
+  print("it is a special character")
