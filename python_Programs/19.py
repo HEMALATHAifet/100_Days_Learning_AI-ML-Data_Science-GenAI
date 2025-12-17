9bb1498b1dbe179a66bdcb462eecaeccdc6fa4fa
A.HEMALATHA <95863646+HEMALATHAifet@users.noreply.github.com>
2025-12-17 15:38:20 +0530

    Add program to sum decimals and compare with PI
    Implement a program to sum two decimal numbers and print if greater than PI.

---
+++ python_Programs/19.py
@@ -0,0 +1,6 @@
+#WAP TO PRINT THE SUM OF 2 DECIMAL NUMBERS IN NON DECIMAL FORMAT, IF THE SUM OF 2 DECIMAL NUMBER IS GREATER THAN THE PI VALUES , ELSE IGNORE
+num1=float(input("enter num1: "))
+num2=float(input("enter num2: "))
+sum=num1+num2
+if sum>3.14:
+  print("sum of ",num1,"and",num2,"is",sum)
