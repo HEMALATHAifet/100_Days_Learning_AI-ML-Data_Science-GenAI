d980c5469d4ec12203b0de0d13e09759c03f546b
A.HEMALATHA <95863646+HEMALATHAifet@users.noreply.github.com>
2025-12-17 17:30:59 +0530

    Add program to find relations between two numbers
    Implement a program to compare two numbers and determine their relationship.

---
+++ python_Programs/40.py
@@ -0,0 +1,11 @@
+#WAP TO FIND THE RELATIONS BETWEEN THE TWO NUMBERS
+num1=int(input("enter num1: "))
+num2=int(input("enter num2: "))
+if num1==num2:
+ print("num1 and num2 are equal")
+elif num1>num2:
+ print("num1 is greater than num2")
+elif num1<num2:
+ print("num1 is lesser than num2")
+else:
+ print("num1 and num2 are not related1")
