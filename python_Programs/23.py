85231a194fa27687a431fd91703e1709d1cdd668
A.HEMALATHA <95863646+HEMALATHAifet@users.noreply.github.com>
2025-12-17 15:55:30 +0530

    Create a program to display 'Hello' or 'Bye'
    Add a program to check if a number is a multiple of 5.

---
+++ python_Programs/23.py
@@ -0,0 +1,6 @@
+#WAP TO DISPLAY 'HELLO', IF A NUMBER ENTERED BY USER IS A MULTIPLE OF 5 ,OTHERWISE PRINT 'BYE'
+num=int(input("enter a number: ")
+if num%5==0:
+        print("it is multiple of 5")
+else:
+        print("Bye")
