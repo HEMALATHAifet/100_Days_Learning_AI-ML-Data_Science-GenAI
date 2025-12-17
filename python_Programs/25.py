323d4542d36eddb46d28c824fc27c1e60d576872
A.HEMALATHA <95863646+HEMALATHAifet@users.noreply.github.com>
2025-12-17 15:59:57 +0530

    Create age eligibility checker for voting
    Add a program to check voting eligibility based on age.

---
+++ python_Programs/25.py
@@ -0,0 +1,5 @@
+#WAP TO READ THE AGE OF A CANDIDATE AND DETERMINE WHETHER THE CANDIDATE IS ELIGIBLE TO CAST HIW OWN VOTE
+age=int(input("enter a number:")
+if age>=18:
+        print("you are eligible to vote")
+        
