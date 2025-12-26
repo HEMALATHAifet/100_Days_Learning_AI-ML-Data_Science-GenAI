697a708aaff893e5fb15ed77684f42658f9fe8cf
A.HEMALATHA <95863646+HEMALATHAifet@users.noreply.github.com>
2025-12-26 14:44:04 +0530

    find largest of 4 numbers

---
+++ python_Programs/60.py
@@ -0,0 +1,13 @@
+#find largest of 4 numbers
+n1=2
+n2=3
+n3=6
+n4=9
+if n1>n2 and n1>n3 and n1>n4:
+  print(n1,"is greatest")
+elif n2>n1 and n2>n3 and n2>n4:
+  print(n2,"is greatest")
+elif n3>n1 and n3>n2 and n3>n4:
+  print(n3,"is greatest")
+else:
+  print(n4,"is greatest")
