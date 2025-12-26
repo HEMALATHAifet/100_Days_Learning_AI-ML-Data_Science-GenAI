5f57b7a44e680b1019c1a2533a6fa9227a759f4c
A.HEMALATHA <95863646+HEMALATHAifet@users.noreply.github.com>
2025-12-26 14:40:59 +0530

    find largest of 3 numbers

---
+++ python_Programs/59.py
@@ -0,0 +1,10 @@
+#find largest of 3 numbers
+n1=int(input("enter value for n1: "))
+n2=int(input("enter value for n2: "))
+n3=int(input("enter value for n3: "))
+if n1>n2 and n1>n3:
+  print(n1,"is greatest")
+elif n2>n1 and n2>n3:
+  print(n2,"is greatest")
+else:
+  print(n3,"is greatest")
