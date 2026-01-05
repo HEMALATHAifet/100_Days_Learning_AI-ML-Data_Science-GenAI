1073649de12cd85b899bbc79baa5f45829ed971c
A.HEMALATHA <95863646+HEMALATHAifet@users.noreply.github.com>
2026-01-05 16:42:23 +0530

    find factorial of a number

---
+++ python_Programs/67.py
@@ -0,0 +1,11 @@
+# find factorial of a number
+num = int(input("Enter a number: "))
+
+fact = 1
+
+if num < 0:
+    print("Factorial is not defined for negative numbers")
+else:
+    for i in range(1, num + 1):
+        fact *= i
+    print("Factorial of", num, "is", fact)
