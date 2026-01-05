f001930c268dbc85db6724940d63b251ea707f64
A.HEMALATHA <95863646+HEMALATHAifet@users.noreply.github.com>
2026-01-05 16:46:17 +0530

    find reverse(without using slicing)

---
+++ python_Programs/70.py
@@ -0,0 +1,10 @@
+# find reverse of a number(without using slicing)
+num = int(input("Enter a number: "))
+rev = 0
+
+while num > 0:
+    digit = num % 10
+    rev = rev * 10 + digit
+    num //= 10
+
+print("Reverse of the number is:", rev)
